d9186ce0ccd1c1b868577713b26209c4a481d398
PFNOW <1609828557@qq.com>
2024-11-22 21:36:49 +0800

    Rebase from original branch v0.5.

--- src/config.py
+++ src/config.py
@@ -1,17 +1,25 @@
 import json
 import os
-
 class Config:
     def __init__(self):
+        self.update_interval = None
+        self.subscriptions_file = None
+        self.notification_settings = None
+        self.openAI_token=os.getenv("OPENAI_TOKEN")
+        self.github_token = os.getenv("GITHUB_TOKEN")
+        self.openAI_URL=os.getenv("OPENAI_URL")
+
         self.load_config()
     
     def load_config(self):
-        # 尝试从环境变量获取配置或使用 config.json 文件中的配置作为回退
         with open('config.json', 'r') as f:
             config = json.load(f)
-            
-            # 使用环境变量或配置文件的 GitHub Token
-            self.github_token = os.getenv('GITHUB_TOKEN', config.get('github_token'))
+
+            # 如果环境变量中没有GitHub Token，则从配置文件中读取
+            if not self.github_token:
+                self.github_token = config.get('github_token')
+
+            self.notification_settings = config.get('notification_settings')
 
             # 初始化电子邮件设置
             self.email = config.get('email', {})
@@ -22,4 +30,4 @@ class Config:
             # 默认每天执行
             self.freq_days = config.get('github_progress_frequency_days', 1)
             # 默认早上8点更新 (操作系统默认时区是 UTC +0，08点刚好对应北京时间凌晨12点)
-            self.exec_time = config.get('github_progress_execution_time', "08:00") 
+            self.exec_time = config.get('github_progress_execution_time', "08:00")
